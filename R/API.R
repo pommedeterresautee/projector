654d93dd8e31b485fe0ce48b4ac732657888ed23
pommedeterresautee <pommedeterresautee@msn.com>
2017-11-27 13:54:35 +0100

    change scope save load model

--- R/API.R
+++ R/API.R
@@ -244,6 +244,7 @@ plot_texts <- function(coordinates, min_cluster_size = 5) {
 #' @param path_annoy path for the [RcppAnnoy] model
 #' @param path_dictionary path for the dictionary ([character] containing texts)
 #' @importFrom assertthat assert_that is.string
+#' @export
 save_annoy_model <- function(annoy_model, path_annoy, path_dictionary) {
   assert_that(is(annoy_model, "Rcpp_AnnoyAngular"))
   assert_that(annoy_model$getNItems() > 0)
@@ -263,6 +264,7 @@ save_annoy_model <- function(annoy_model, path_annoy, path_dictionary) {
 #' @param path_annoy path to the [RcppAnnoy] model
 #' @param path_dictionary path to the dictionary ([character] containing texts)
 #' @importFrom assertthat assert_that is.string
+#' @export
 load_annoy_model <- function(path_annoy, path_dictionary) {
   assert_that(is.string(path_annoy))
   assert_that(is.string(path_dictionary))
